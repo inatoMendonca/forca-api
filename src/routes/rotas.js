991de278f355504582e3a11f013d96c03f0d211c
Renato Jean V. Christino <48481257+inatoMendonca@users.noreply.github.com>
2020-01-23 12:03:51 -0300

    alteracao rotas

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -7,7 +7,7 @@ const router = express();
 
 console.log('Enabling CORS');
 let allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     if ('OPTIONS' === req.method) {
